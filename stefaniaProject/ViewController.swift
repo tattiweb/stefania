623ee66d7a437136ab25d0a6ba148aa28b77a506
Stefania <tattiweb@gmail.com>
2017-03-10 12:12:36 +0100

    fix

--- stefaniaProject/ViewController.swift
+++ stefaniaProject/ViewController.swift
@@ -12,7 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("viva Antonio!")
+        print ("Viva Fabio")
+      
         // Do any additional setup after loading the view, typically from a nib.
     }
 
